286fcc014e387f7e2dd803d4617b9289dd5dce06
whentea <wentyyuniarti@gmail.com>
2018-11-12 16:41:15 +0700

    define models and views

--- akademik/models.py
+++ akademik/models.py
@@ -3,3 +3,12 @@
 # Create your models here.
 from mongoengine import *
 connect('datakoe', host='localhost')
+
+class StatusModel(EmbeddedDocument):
+    status_text = StringField(max_length=200)
+    votes = IntField(default=0)
+
+class userModel(Document):
+    nama = STringField(max_length=200)
+    tgl = DateTimeField(help_text='date published')
+    status = ListField(EmbeddedDocumentField(StatusModel))
--- akademik/views.py
+++ akademik/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 
 # Create your views here.
+
+from mhs.models import UserModel, StatusModel
+import datetime
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+# Create your views here.
+def index(request):
+    data = {}
+    return render(request,"mhs/index.html",data)
+
+
+def create(request):
+    nilai1 = StatusModel(choice_text="bahasa indonesia", votes=90)
+    nilai2 = StatusModel(choice_text="bahasa inggris", votes=72)
+    nilai3 = StatusModel(choice_text="bahasa arab", votes=69)
+    nilai4 = StatusModel(choice_text="bahasa jawa", votes=81)
+
+    nilais = [nilai1, nilai2, nilai3, nilai4]
+
+    user = UserModel(nama="Wahyu", pub_date=datetime.datetime.now(), nilais=nilais)
+    user.save()
+
+    user = UserModel(nama="Wenty", pub_date=datetime.datetime.now(),
+                     nilais=nilais)
+    user.save()
+
+    return HttpResponseRedirect(reverse("mhs:show"))
+
+
+def show(request):
+    data = {}
+    p = UserModel.objects.all()
+    data["users"] = p
+    return render(request, "mhs/show.html", data)
+
+
+def delete(request, document_id):
+    UserModel.objects.filter(id=document_id).delete()
+    return HttpResponseRedirect(reverse("mhs:show"))
